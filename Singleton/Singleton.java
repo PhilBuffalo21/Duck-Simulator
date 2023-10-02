2eee4333dc1afa186dc2bd6c9d9f7ca1ce9a8ca3
KrisTheMuggle <kris.themuggle@gmail.com>
2023-10-02 14:50:31 +1100

    Adds Singleton Pattern showcase

---
+++ Singleton/Singleton.java
@@ -0,0 +1,16 @@
+package Singleton;
+
+public class Singleton {
+    private static Singleton instance;
+
+    private Singleton() {
+
+    }
+
+    public Singleton getInstance() {
+        if (instance == null) {
+            instance = new Singleton();
+        }
+        return instance;
+    }
+}
